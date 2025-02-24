ebf6e346dcbadc90dcaca114f14b658e4ac5b1f9
CaciaRodrigues <caciarodriguues@gmail.com>
2025-02-24 14:30:29 -0300

    Mudando o endereço do endpoint

--- src/components/QRCodeGenerator.tsx
+++ src/components/QRCodeGenerator.tsx
@@ -9,7 +9,7 @@ export default function QRCodeGenerator() {
     const [qrCode, setQRCode] = useState<string>('');
 
     const generateQRCode = async () => {
-        const response = await fetch('http://127.0.0.1:5000/generate_qr', {
+        const response = await fetch('https://qrcode-generator-c4t1.onrender.com', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ text })
