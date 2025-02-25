1e2128cc1d5b6e90bffa0ee5935346f3f80a5d85
CaciaRodrigues <caciarodriguues@gmail.com>
2025-02-25 15:30:22 -0300

    Melhorando responsividade

--- src/components/QRCodeGenerator.tsx
+++ src/components/QRCodeGenerator.tsx
@@ -9,23 +9,22 @@ export default function QRCodeGenerator() {
     const [qrCode, setQRCode] = useState<string>('');
 
     const generateQRCode = async () => {
-        const backendURL = process.env.REACT_APP_BACKEND_URL || 'https://qrcode-generator-c4t1.onrender.com';
+        const backendURL = 'http://127.0.0.1:5000';
 
         try {
-            const response = await fetch(`${backendURL}/generate_qr`, {  // Incluindo o endpoint correto
+            const response = await fetch(`${backendURL}/generate_qr`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ text })
             });
-    
-            // Verificar se a resposta é bem-sucedida (código 200)
+
             if (!response.ok) {
                 console.error('Failed to fetch QR code:', response.statusText);
                 return;
             }
-    
+
             const data: QRCodeResponse = await response.json();
-    
+
             if (data.qr_code) {
                 setQRCode(`data:image/png;base64,${data.qr_code}`);
             } else {
@@ -35,27 +34,25 @@ export default function QRCodeGenerator() {
             console.error('Error during QR code generation:', error);
         }
     };
-    
-    
 
     return (
-        <div className="flex items-center justify-center h-screen bg-dark-color p-3">
-            <div className="w-2/5 h-1/2 bg-mid-lit-color shadow-lg rounded-2xl overflow-hidden">
+        <div className="w-full h-screen bg-dark-color p-0  flex justify-center items-center">
+            <div className="w-full h-full sm:w-2/5 sm:h-1/2 bg-mid-lit-color shadow-lg sm:rounded-2xl overflow-hidden sm:m-auto">
                 {/* Título */}
                 <div className="text-center p-2 border-b border-mid-color">
                     <h1 className="text-2xl font-bold text-gray-50">QR Code Generator</h1>
                 </div>
 
-                <div className="grid grid-cols-2 gap-4 h-full bg-light-color">
-                    {/* Esquerda - Input e Botão */}
-                    <div className="flex flex-col justify-center items-center space-y-6 px-5 h-full">
+                <div className="flex flex-col sm:grid sm:grid-cols-2 h-full">
+                    {/* Input e Botão */}
+                    <div className="flex flex-col justify-center items-center space-y-4 px-5 py-4 sm:space-y-6 sm:px-5 sm:h-full sm:border-r border-mid-color h-[35%] sm:w-full">
                         <input
                             className="w-full max-w-md border border-mid-color p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                             value={text}
                             onChange={(e) => setText(e.target.value)}
                             placeholder="Type your text here"
                         />
-                        <button 
+                        <button
                             className="w-full max-w-md bg-pop-color text-gray-50 p-3 rounded-lg hover:bg-dark-pop-color transition-all"
                             onClick={generateQRCode}
                         >
@@ -63,14 +60,14 @@ export default function QRCodeGenerator() {
                         </button>
                     </div>
 
-                    {/* Direita - QR Code */}
-                    <div className="flex items-center justify-center text-center bg-mid-color p-6 h-full">
+                    {/* QR Code */}
+                    <div className="flex items-center justify-center text-center bg-mid-color p-6 h-[65%] sm:h-full">
                         {qrCode ? (
                             <div className="flex flex-col items-center">
-                                <img src={qrCode} alt="QR Code" className="w-3/4 h-3/4 object-contain" />
-                                <a 
-                                    href={qrCode} 
-                                    download="qr-code.png" 
+                                <img src={qrCode} alt="QR Code" className="w-48 h-48 object-contain" />
+                                <a
+                                    href={qrCode}
+                                    download="qr-code.png"
                                     className="mt-4 bg-pop-color text-gray-50 p-3 rounded-lg hover:bg-dark-pop-color transition-all"
                                 >
                                     Save QR Code
